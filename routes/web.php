cb9dfdaf8fbbce5e7d36bb0306d91ea6935b47a1
Andrea9808 <andrea.manigrasso98@gmail.com>
2024-02-12 14:58:40 +0100

    problem: .View [.] not found

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Guest\PageController;
 
 Route::get('/', function () {
 
-    return view('pages.index');
+    return view('/', [PageController::class, 'index']);
 });
